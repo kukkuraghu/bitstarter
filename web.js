2146713a70ef76da09d1c912d619816c38169c57
kukkuraghu <raghu.ned@gmail.com>
2013-07-22 08:49:27 +0000

    Updated index.html and web.js for my bitstarter.Added image files

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
+//var app = express();
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function(request, response) {
+  console.log(request.headers['user-agent']);
   response.send(fs.readFileSync("index.html").toString());
 });
 
